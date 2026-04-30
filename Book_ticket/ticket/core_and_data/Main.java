1cb69a763780edeb75879acf093b9a0f5779d29d
Abdelrahman Elshrif <A.Adel2427@nu.edu.eg>
2026-04-30 16:32:47 +0300

    Add files via upload

---
+++ Book_ticket/ticket/core_and_data/Main.java
@@ -0,0 +1,10 @@
+package core_and_data;
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Login L = new Login();
+        L.fillLogin();
+    }
+}
